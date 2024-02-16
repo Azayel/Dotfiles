a4d275fb61d1947e3a470a3b007f58762907f7e2
Azayel <Asayel404@proton.me>
2024-02-16 20:42:32 +0100

    minor changes

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,13 +1,14 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  build = ":TSUpdate",
-  config = function()   
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = { "cpp", "lua", "vim", "glsl", "java", "make", "latex", "cmake", "rust"},
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },  
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			auto_install = true,
+			ensure_installed = { "cpp", "lua", "vim", "glsl", "java", "make", "latex", "cmake", "rust" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
